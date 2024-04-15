0ccd6861c876e878e7c6668955ff4609bfde23e4
KRE-95 <68148527+KRE-95@users.noreply.github.com>
2024-04-15 16:10:20 +0500

    settup vite up to tailwind
    Signed-off-by: KRE-95 <68148527+KRE-95@users.noreply.github.com>

---
+++ 03tailwindprop/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='text-3xl bg-green-500 p-3 rou'>Vite with tailwind</h1>
+     
+    </>
+  )
+}
+
+export default App

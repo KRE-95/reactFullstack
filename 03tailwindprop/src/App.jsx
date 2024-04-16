b59184136a0cd2e15ddd924902693b3ee3344136
KRE-95 <68148527+KRE-95@users.noreply.github.com>
2024-04-16 20:42:42 +0500

    Added Card component styling with tailwind
    Signed-off-by: KRE-95 <68148527+KRE-95@users.noreply.github.com>

--- 03tailwindprop/src/App.jsx
+++ 03tailwindprop/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
  
 
   return (
     <>
-    <h1 className='text-3xl bg-green-500 p-3 rou'>Vite with tailwind</h1>
+    <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with tailwind</h1>
+    <Card/>
      
     </>
   )
